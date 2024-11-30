9715e92495a239ce73426ed64770f70b6e7b1d2a
Alice Agnoletto <alicelulu.agnoletto@gmail.com>
2024-11-30 17:40:02 -0600

    map api

--- routes/home.js
+++ routes/home.js
@@ -3,38 +3,6 @@ import { Router } from "express";
 const router = Router()
 
 router.route('/').get(async (req, res) => {
-  res.render('../views/account');
-});
-
-router.route('/home').get(async (req, res) => {
-  res.render('../views/home');
-});
-
-router.route('/map').get(async (req, res) => {
-  res.render('../views/map');
-});
-
-router.route('/wifi-locations').get(async (req, res) =>{
-  try{
-    const wifiLocations = await getWifiLocations();
-    res.json(wifiLocations);
-  }catch (e){
-    res.status(500).json({ error: 'Failed to fetch Wi-Fi locations' });
-  }
-});
-
-
-router.route('/wifi-review').post(async (req, res) => {
-  let score = req.body.rating;
-  let text = req.body.text;
-  let id = req.body.id;
-  //console.log(score, text);
-  try{
-    const wifiReview = await createWifiReview(score, text,id);
-    res.json(wifiReview);
-  }catch (e){
-    res.status(500).json({error: e});
-  }
   res.render('../views/home', { title: "WiFly NYC" });
 });
 
