af78c17c68c34f16c51e6234b2695797db64d864
Christopher Engelbart <cengelbart39@protonmail.com>
2024-12-03 01:19:05 -0500

    Add sample image upload page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ import aboutRoutes from './about.js';
 import coffeeShopRoutes from './coffeeShop.js';
 import eventRoutes from './events.js';
 import feedRoutes from './feed.js';
-import homeRoutes from './home.js';
 import locationRoutes from './location.js';
 import loginRoutes from './login.js';
-import reviewRoutes from './review.js';
 import mapRoutes from './map.js';
+import reviewRoutes from './review.js';
 
 const constructorMethod = (app) => {
 
@@ -21,11 +20,19 @@ const constructorMethod = (app) => {
   app.use('/location', locationRoutes);
   app.use('/coffeeShop', coffeeShopRoutes);
   app.use('/home', mapRoutes);
-  
+
   app.use('/public', staticDir('public'));
 
+  app.use('/upload', (req, res) => {
+    res.render('../views/imageUpload', {
+      title: "Upload Image",
+      CloudName: JSON.stringify(process.env.CLOUDINARY_CLOUD_NAME),
+      Preset: JSON.stringify(process.env.CLOUDINARY_UPLOAD_PRESET)
+    });
+  });
+
   app.use('*', (req, res) => {
-    res.status(404).json({error: 'Resource not found'});
+    res.status(404).json({ error: 'Resource not found' });
   });
 };
 
