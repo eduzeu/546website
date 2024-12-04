b16dcc9ad7959c792a25d3b70f5ba7759803a38a
Christopher Engelbart <cengelbart39@protonmail.com>
2024-12-03 19:57:46 -0500

    Fix issue that caused app to stall on start

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,23 @@ import aboutRoutes from './about.js';
 import coffeeShopRoutes from './coffeeShop.js';
 import eventRoutes from './events.js';
 import feedRoutes from './feed.js';
-import homeRoutes from './home.js';
 import locationRoutes from './location.js';
 import loginRoutes from './login.js';
 import reviewRoutes from './review.js';
 import mapRoutes from './map.js';
 
 const constructorMethod = (app) => {
-
-  app.use("/", loginRoutes);
-  // app.use("/home", homeRoutes);
   app.use('/feed', feedRoutes);
   app.use('/events', eventRoutes);
   app.use('/about', aboutRoutes);
-  app.use('/login', loginRoutes);
   app.use('/review', reviewRoutes);
   app.use('/location', locationRoutes);
   app.use('/coffeeShop', coffeeShopRoutes);
   app.use('/home', mapRoutes);
   
   app.use('/public', staticDir('public'));
+
+  app.use("/", loginRoutes);
 
   app.use('*', (req, res) => {
     res.status(404).json({error: 'Resource not found'});
--- routes/login.js
+++ routes/login.js
@@ -1,39 +1,38 @@
 import { Router } from "express";
 import * as userFunctions from "../data/users.js";
 
-const router = Router()
+const router = Router();
 
-router.route('/').get(async (req, res) => {
-    res.render('../views/account');
-})
-
-router.route('/').post(async (req, res) => {
+router.route("/")
+  .get(async (req, res) => {
+    res.render("../views/account");
+  })
+  .post(async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const result = await userFunctions.checkUser(username, password);
-        if(result){
-            res.status(200).json(result);
-        }
-        else{
-            throw 'Invalid Login';
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.toString() });
-    }
-});
-
-router.route('/newAccount').get(async (req, res) => {
-    res.render('../views/newAccount');
-})
-
-router.route('/newAccount').post(async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const result = await userFunctions.addNewUser(username, email, password);
+      const { username, password } = req.body;
+      const result = await userFunctions.checkUser(username, password);
+      if (result) {
         res.status(200).json(result);
+      } else {
+        throw "Invalid Login";
+      }
     } catch (error) {
-        res.status(400).json({error: error.toString()});
+      res.status(400).json({ error: error.toString() });
     }
-});
+  });
+
+router.route("/newAccount")
+  .get(async (req, res) => {
+    res.render("../views/newAccount");
+  })
+  .post(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const result = await userFunctions.addNewUser(username, email, password);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: error.toString() });
+    }
+  });
 
 export default router;
