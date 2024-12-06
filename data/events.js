c34208d799b48d230121d642f1517c0f38121f8b
Emma Freire <emmafreire@Lion8s-MacBook-Pro.local>
2024-12-06 15:49:49 -0500

    new and improved events page

--- data/events.js
+++ data/events.js
@@ -5,8 +5,28 @@ export const getAllEvents = async ()=> {
     let response = await axios.get(`https://data.cityofnewyork.us/resource/tvpp-9vvx.json`);
 
     let data = response.data
+
+    let results = []
     
-    return data
+
+    for (const event of data) {
+        if(results.length >= 50){
+            break;
+        }
+        let eventDates = event.start_date_time;
+        eventDates = eventDates.split("T");
+
+        eventDates = eventDates[0]
+        eventDates = eventDates.split("-");
+
+        eventDates = eventDates[1] +"/"+ eventDates[2] +"/"+ eventDates[0] 
+        event.start_date_time = eventDates
+
+        results.push(event);
+        
+    }
+    
+    return results 
 }
 
 export const getEventbyBorough = async (city) => {
@@ -15,7 +35,21 @@ export const getEventbyBorough = async (city) => {
     let events_in_borough = [];
 
     for (const event of data) {
+        if(events_in_borough.length >= 50){
+            break;
+        }
+
         if (event.event_borough === city) {
+            let eventDates = event.start_date_time;
+            eventDates = eventDates.split("T");
+
+            eventDates = eventDates[0]
+            eventDates = eventDates.split("-");
+
+            eventDates = eventDates[1] +"/"+ eventDates[2] +"/"+ eventDates[0] 
+            event.start_date_time = eventDates
+
+
             events_in_borough.push(event);
         }
     }
@@ -28,10 +62,21 @@ export const getEventbyDate = async (date) => {
     let events_in_date = [];
 
     for (const event of data) { 
+        if(events_in_date.length >= 50){
+            break;
+        }
+
         let eventDates = event.start_date_time;
         eventDates = eventDates.split("T");
 
-        if (eventDates[0] === date) {
+        eventDates = eventDates[0]
+        eventDates = eventDates.split("-");
+
+        eventDates = eventDates[1] +"/"+ eventDates[2] +"/"+ eventDates[0] 
+        
+
+        if (eventDates == date) {
+            event.start_date_time = eventDates
             events_in_date.push(event);
         }
     }
@@ -39,7 +84,7 @@ export const getEventbyDate = async (date) => {
 }
 
 
-//console.log(await getEventbyDate("2024-11-27"));
+//console.log(await getEventbyDate("11/27/2024"));
 
 //console.log(await getEventbyBorough("Queens"));
 
--- routes/events.js
+++ routes/events.js
@@ -1,47 +1,62 @@
 import { Router } from "express";
-import { getAllEvents, getEventbyDate } from '../data/events.js';
+import { getAllEvents, getEventbyDate, getEventbyBorough } from '../data/events.js';
 
 const router = Router()
 
 router.route("/")
     .get(async (req, res) => {
-        try{
+        try {
             let allEvents = await getAllEvents();
-            res.render('events', {'title':'Events', 'events': allEvents});
-          }catch(e){
+            res.render('events', { 'title': 'Events', 'events': allEvents });
+        } catch (e) {
             return res.status(400).send(e);
-          }
+        }
     })
 
 router.route("/city")
     .get(async (req, res) => {
-        try{
-            res.render('city', {'title':'Events by Bourough'});
-        }catch(e){
-            return res.status(400).send(e);
-        }
-    }).post(async (req, res) => {
-        const searchDate = req.body.searchByDate;
         try {
-            if (!searchDate) throw 'You must provide a date to search for';
-            const events = await getEventbyDate(searchDate);
-            
-            return res.render('dates', {title: 'Search Events by Date',events: events, searchDate: searchDate
-            });
-        }catch{ 
-            return res.status(400).send(e);
-        }
-    });
-    
-router.route("/date")
-    .post(async (req, res) => {
-        let searchDate = req.body.searchByDate;
-        try{
-            let dates = await getEventbyDate(searchDate);
-            res.render('dates', {'title':'Events by Date', 'events': dates});
-        }catch(e){
+            res.render('cities', { 'title': 'Events by Borough' });
+        } catch (e) {
             return res.status(400).send(e);
         }
     })
+    .post(async (req, res) => {
+        const searchBorough = req.body.searchByBorough;
+        try {
+            if (!searchBorough) throw 'You must provide a borough to search for';
+            const events = await getEventbyBorough(searchBorough);
+            return res.render('results', {
+                title: 'Search Events by Borough',
+                events: events,
+                searchBorough: searchBorough
+            });
+        } catch (e) {
+            return res.status(400).send(e);
+        }
+    });
+
+router.route("/date")
+    .get(async (req, res) => {
+        try {
+            res.render('dates', { 'title': 'Events by Date' });
+        } catch (e) {
+            return res.status(400).send(e);
+        }
+    })
+    .post(async (req, res) => {
+        const searchDate = req.body.searchByDate;
+        try {
+            if (!searchDate) throw 'You must provide a date to search for in format mm/dd/yyyy';
+            const events = await getEventbyDate(searchDate);
+            return res.render('results', {
+                title: 'Search Events by Date',
+                events: events,
+                searchDate: searchDate
+            });
+        } catch (e) {
+            return res.status(400).send(e);
+        }
+    });
 
 export default router;
