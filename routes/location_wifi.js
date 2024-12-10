f80f97b5c0331c12dd4b95e17793591755be2802
Christopher Engelbart <cengelbart39@protonmail.com>
2024-12-09 23:58:39 -0500

    Fix some bugs

--- routes/location_wifi.js
+++ routes/location_wifi.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getWifiLocations } from "../data/locations.js";
+import { fetchCoffeeShops, getWifiLocations } from "../data/locations.js";
 
 const router = Router()
 
