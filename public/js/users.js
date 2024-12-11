6b0a61ebef7f8a160287669a54395f3cdec8f514
Daniel Fong <dkf2003@gmail.com>
2024-12-10 19:53:57 -0500

    Added refreshing cookies

--- public/js/users.js
+++ public/js/users.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('accountHandling');
 const username = document.getElementById('loginUser');
 const password = document.getElementById('loginPassword');
+const confirmPassword = document.getElementById('confirmPassword');
 const email = document.getElementById('loginEmail');
 const error = document.getElementById('error');
 
@@ -9,8 +10,12 @@ if (form) {
         e.textContent = "";
 
         try {
+            confirmPassword.value = validateString(confirmPassword.value, 'Confirm-Password');
             username.value = validateString(username.value, 'Username');
             password.value = validateString(password.value, 'Password');
+            if(confirmPassword.value !== password.value){
+                throw 'Passwords do not match';
+            }
             if (email) {
                 email.value = validateEmailAddress(email.value, 'Email');
             };
