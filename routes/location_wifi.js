6c4b645127f6f86af63e4cda27414a167df5f4e3
Christopher Engelbart <cengelbart39@protonmail.com>
2024-12-10 19:20:11 -0500

    Fix syntax error

--- routes/location_wifi.js
+++ routes/location_wifi.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { fetchCoffeeShops, getWifiLocations } from "../data/locations.js";
-import { getWifiLocations } from "../data/locations.js";
 import * as sessionTokens from "../data/sessionTokens.js";
 
 
