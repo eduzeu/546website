52ac2b0c7d7e3b358e85571740f949fac674a1bc
Christopher Engelbart <cengelbart39@protonmail.com>
2024-12-14 16:11:41 -0500

    Add Friends Data Validation

--- data/friendlist.js
+++ data/friendlist.js
@@ -1,11 +1,8 @@
 import * as userData from "../config/mongoCollections.js";
+import { validateString } from "../helpers.js";
 
 export const getAllFriends = async (name) => {
-    if (!name) throw 'Name is required';
-    if (typeof name !== 'string') throw 'User Id must be a string';
-    if (name.trim().length === 0) throw 'User Id cannot be empty or just spaces';
-
-    name = name.trim().toLowerCase();
+    name = validateString(name, "Username").toLowerCase();
 
     const data = await userData.users();
     const user = await data.findOne({username: name});
